fa6d6248bb469b56238fe4eda0505bb414cf16c0
adam0012af-ai <adam0012af@gmail.com>
2026-09-05 10:01:05 +0300

    Add editable reel template model

---
+++ app/src/main/java/com/adam/downloadhub/ReelTemplate.java
@@ -0,0 +1,42 @@
+package com.adam.downloadhub;
+
+import java.io.Serializable;
+
+public final class ReelTemplate implements Serializable {
+    public final String id;
+    public final String categoryKey;
+    public final String categoryName;
+    public final String name;
+    public final String hook;
+    public final String body;
+    public final String cta;
+    public final String hashtags;
+    public final String layout;
+    public final String motion;
+    public final String captionStyle;
+    public final int startColor;
+    public final int endColor;
+    public final int durationSec;
+    public final boolean islamic;
+
+    public ReelTemplate(String id, String categoryKey, String categoryName, String name,
+                        String hook, String body, String cta, String hashtags,
+                        String layout, String motion, String captionStyle,
+                        int startColor, int endColor, int durationSec, boolean islamic) {
+        this.id = id;
+        this.categoryKey = categoryKey;
+        this.categoryName = categoryName;
+        this.name = name;
+        this.hook = hook;
+        this.body = body;
+        this.cta = cta;
+        this.hashtags = hashtags;
+        this.layout = layout;
+        this.motion = motion;
+        this.captionStyle = captionStyle;
+        this.startColor = startColor;
+        this.endColor = endColor;
+        this.durationSec = durationSec;
+        this.islamic = islamic;
+    }
+}
